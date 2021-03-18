0cd1edf84c398f058b0962f437acdbcbd784f9dc
vnfedotov <vfl@ispras.ru>
2021-03-18 16:54:01 +0300

    second problemMatcher

--- task.sh
+++ task.sh
@@ -2,4 +2,6 @@
 
 sleep 1
 
-echo P1/src/main.c:18:10: fatal error: string.h: No such file or directory
+echo "P1/src/main.c:18:10: fatal error: string.h: No such file or directory"
+
+echo "TypeScript error: main.ts(118,30): Error TS2339: Property 'object' does not exist on type 'boolean'."
